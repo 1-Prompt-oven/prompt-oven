8097bf53338cbfdc0b77f166a6da2cc2701073a0
subin <osb4469@gmail.com>
2024-11-26 17:07:28 +0900

    Feat: 카트 기능 컴포넌트 구조 설계 및 더미 데이터, 타입 선언

--- apps/client/src/app/cart/page.tsx
+++ apps/client/src/app/cart/page.tsx
@@ -1,7 +1,23 @@
 import React from "react"
+import type { CartItemType } from "@/types/cart/cartTypes"
+import { getCartData } from "@/action/cart/getCartData"
+import { calculateTotalPrice } from "@/action/cart/cartDataAction"
+import CartItemContainer from "@/components/cart/organism/CartItemContainer"
+import CartCheckout from "@/components/cart/organism/CartCheckout"
 
-function cart() {
-	return <div>Cart</div>
+async function cart() {
+	const items: CartItemType[] = await getCartData()
+	// 추후 uuid 가지고 상품 데이터 가져와서 만든 최종 사용자 장바구니 데이터를 items 에 재할당해서
+	// 컴포넌트에 내려줘야 함.
+
+	const selectedItemsTotalPrice = await calculateTotalPrice(items)
+
+	return (
+		<main>
+			<CartItemContainer items={items} />
+			<CartCheckout totalPrice={selectedItemsTotalPrice} />
+		</main>
+	)
 }
 
 export default cart
