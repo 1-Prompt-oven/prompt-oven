cb9f6fdbc2ea77505419bcf91a856e84941890f0
subin <osb4469@gmail.com>
2024-11-26 12:03:56 +0900

    Design: dialog 너비 수정 및 loading 상태 따른 스켈레톤 렌더링 설정

--- apps/client/src/components/common/atom/SearchItemSkeleton.tsx
+++ apps/client/src/components/common/atom/SearchItemSkeleton.tsx
@@ -6,12 +6,12 @@ function SearchItemSkeleton() {
 		<div className="space-y-4">
 			{[1, 2, 3].map((i) => (
 				<div className="flex items-center gap-3 p-2" key={`prompt-${i}`}>
-					<Skeleton className="h-16 w-16 rounded" />
+					<Skeleton className="h-[4rem] w-[4rem] rounded" />
 					<div className="flex-1 space-y-2">
-						<Skeleton className="h-4 w-[200px]" />
+						<Skeleton className="h-[1rem] w-[60%] rounded-none" />
 						<div className="flex items-center gap-2">
-							<Skeleton className="h-3 w-[100px]" />
-							<Skeleton className="h-3 w-[50px]" />
+							<Skeleton className="h-[0.75rem] w-[20%] rounded-none" />
+							<Skeleton className="h-[0.75rem] w-[10%] rounded-none" />
 						</div>
 					</div>
 				</div>
