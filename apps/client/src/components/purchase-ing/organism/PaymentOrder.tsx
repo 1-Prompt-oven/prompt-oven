7aa22cc4889dcf79ea9f4862fd14a1732cf81fb8
jangdebug <jangdebug@naver.com>
2024-11-26 17:58:59 +0900

    Payment button publishing completed

--- apps/client/src/components/purchase-ing/organism/PaymentOrder.tsx
+++ apps/client/src/components/purchase-ing/organism/PaymentOrder.tsx
@@ -1,3 +1,33 @@
+import { Button } from "@repo/ui/button"
+
 export default function PaymentOrder() {
-	return <div className="h-[300px] w-[300px] rounded-md bg-white"></div>
+	return (
+		<div className="flex h-full w-[300px] flex-col gap-4 rounded-md bg-white p-4 text-sm">
+			<p className="font-semibold">Message</p>
+			<textarea
+				name="paymentMessage"
+				placeholder="Leave a message"
+				className="h-[130px] resize-none rounded-lg border border-[#d8d8d8] p-3"
+			/>
+
+			<div className="flex flex-col gap-2 border-t-2 border-black pt-4 font-semibold">
+				<div className="flex justify-between">
+					<p>Total Order</p>
+					<p className="flex gap-2">x7 Product</p>
+				</div>
+				<div className="flex justify-between">
+					<p>Total Payment</p>
+					<p className="flex gap-4">
+						<span className="text-xl font-bold text-[#9747ff]">$6460</span>
+					</p>
+				</div>
+			</div>
+
+			<Button
+				type="submit"
+				className="bg-[#9747ff] text-white hover:bg-[#743dbd]">
+				<span>Order Now</span>
+			</Button>
+		</div>
+	)
 }
