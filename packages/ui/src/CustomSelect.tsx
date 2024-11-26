1c94aa8024e1a7292940fec00d0da41bf7b07548
ParkSinGyu <singyu13@gmail.com>
2024-11-26 13:37:06 +0900

    JYXB-320/feature: CustomSelect.tsx font size 수정

--- packages/ui/src/CustomSelect.tsx
+++ packages/ui/src/CustomSelect.tsx
@@ -13,7 +13,7 @@ const SelectGroup = SelectPrimitive.Group
 const SelectValue = SelectPrimitive.Value
 
 const selectTriggerVariants = cva(
-	"flex h-9 text-[0.875rem] leading-[160%] tracking-[-0.02em] w-full items-center justify-between whitespace-nowrap rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-1 focus:ring-ring disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1",
+	"flex h-9 leading-[160%] tracking-[-0.02em] w-full items-center justify-between whitespace-nowrap rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-1 focus:ring-ring disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1",
 	{
 		variants: {
 			variant: {
@@ -40,9 +40,8 @@ const SelectTrigger = React.forwardRef<
 		<SelectPrimitive.Icon asChild>
 			<ChevronDown
 				className={cn(
-					"h-4 w-4 opacity-50",
-					variant === "black" &&
-						"text-white transition-transform duration-200 ease-in-out data-[state=open]:rotate-180",
+					"h-5 w-5 opacity-50 transition-transform duration-200 ease-in-out data-[state=open]:rotate-180",
+					variant === "black" && "text-white",
 				)}
 			/>
 		</SelectPrimitive.Icon>
@@ -61,7 +60,7 @@ const SelectScrollUpButton = React.forwardRef<
 			className,
 		)}
 		{...props}>
-		<ChevronUp className="h-4 w-4" />
+		<ChevronUp className="h-5 w-5" />
 	</SelectPrimitive.ScrollUpButton>
 ))
 SelectScrollUpButton.displayName = SelectPrimitive.ScrollUpButton.displayName
@@ -77,7 +76,7 @@ const SelectScrollDownButton = React.forwardRef<
 			className,
 		)}
 		{...props}>
-		<ChevronDown className="h-4 w-4" />
+		<ChevronDown className="h-5 w-5" />
 	</SelectPrimitive.ScrollDownButton>
 ))
 SelectScrollDownButton.displayName =
@@ -142,7 +141,7 @@ const SelectLabel = React.forwardRef<
 SelectLabel.displayName = SelectPrimitive.Label.displayName
 
 const selectItemVariants = cva(
-	"relative text-[875rem] leading-[160%] tracking-[-0.02em] flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-2 pr-8 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
+	"relative leading-[160%] tracking-[-0.02em] flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-2 pr-8 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
 	{
 		variants: {
 			variant: {
@@ -170,7 +169,7 @@ const SelectItem = React.forwardRef<
 				"absolute right-2 flex h-3.5 w-3.5 items-center justify-center",
 			)}>
 			<SelectPrimitive.ItemIndicator>
-				<Check className="h-4 w-4" />
+				<Check className="h-5 w-5" />
 			</SelectPrimitive.ItemIndicator>
 		</span>
 		<SelectPrimitive.ItemText>{children}</SelectPrimitive.ItemText>
