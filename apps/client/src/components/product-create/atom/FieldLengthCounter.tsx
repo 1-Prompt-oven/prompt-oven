2035d595aacb5bd53a9ebd432897ae2c1fd63e29
ParkSinGyu <singyu13@gmail.com>
2024-11-27 16:27:32 +0900

    JYXB-320/feature: Create Product 컴포넌트 추가 및 수정
    - 추가
     - AccountTitleText.tsx
     - FieldLengthCounter.tsx
     - PcButton.tsx
     - PcDescription.tsx
     - PcSaveBar.tsx
     - PcSelect.tsx
    
    - 수정
     - PcInput.tsx
     - PcTextarea.tsx

---
+++ apps/client/src/components/product-create/atom/FieldLengthCounter.tsx
@@ -0,0 +1,25 @@
+import type { ReactElement } from "react"
+import React from "react"
+
+interface FieldCountWrapperProps {
+	children?: ReactElement
+	maxLength: number
+	length: number
+}
+
+function FieldLengthCounter({
+	children,
+	maxLength,
+	length,
+}: FieldCountWrapperProps) {
+	return (
+		<div className="flex w-full flex-col items-end gap-1">
+			{children}
+			<span className="text-sm font-normal leading-[160%] tracking-[0.04em] text-[#A0A0A0]">
+				{length} / {maxLength} Characters
+			</span>
+		</div>
+	)
+}
+
+export default FieldLengthCounter
--- apps/client/src/components/product-create/atom/PcInput.tsx
+++ apps/client/src/components/product-create/atom/PcInput.tsx
@@ -8,12 +8,12 @@ export const PcInput = forwardRef<HTMLInputElement, CustomInputProps>(
 	({ className, ...props }, ref) => {
 		return (
 			<input
+				ref={ref}
+				{...props}
 				className={cn(
 					"w-full rounded-md border-b border-black bg-black p-[10px_14px] text-[0.875rem] leading-[160%] tracking-[-0.02em] text-slate-400 placeholder:text-slate-400 focus:outline-none",
 					className,
 				)}
-				ref={ref}
-				{...props}
 			/>
 		)
 	},
