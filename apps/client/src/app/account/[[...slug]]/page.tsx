599045ba67080045e2fae7b04ddb262eff53c867
ParkSinGyu <singyu13@gmail.com>
2024-11-28 16:43:06 +0900

    JYXB-304/Feat: CreateProductPage.tsx를 CreateProductFirstPage.tsx로 이름 변경

--- apps/client/src/app/account/[[...slug]]/page.tsx
+++ apps/client/src/app/account/[[...slug]]/page.tsx
@@ -3,14 +3,14 @@ import PurchaseEd from "@/app/purchase/ed/page"
 import PurchaseIng from "@/app/purchase/ing/page"
 import ContentWrapper from "@/components/account/template/ContentWrapper.tsx"
 import type { SearchParams } from "@/types/account/searchParams.ts"
-import CreateProductPage from "@/components/product-create/page/CreateProductPage.tsx"
+import CreateProductFirstPage from "@/components/product-create/page/CreateProductFirstPage.tsx"
 
 export default function page({ searchParams }: SearchParams) {
 	// note: queryParam이 없는 경우 overview 사이드바 메뉴가 선택되게 하기 -- 필요에 따라 수정 필요
 	const view = searchParams.view ?? "overview"
 	return (
 		<ContentWrapper queryParams={{ view }}>
-			{view === "create-product" && <CreateProductPage />}
+			{view === "create-product" && <CreateProductFirstPage />}
 			{view === "favorites" && <Favorite />}
 			{view === "purchase-ongoing" && <PurchaseIng />}
 			{view === "purchase-completed" && <PurchaseEd />}
---
+++ apps/client/src/components/product-create/page/CreateProductFirstPage.tsx
@@ -0,0 +1,166 @@
+"use client"
+
+import { Controller, useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import AccountTitleText from "@/components/common/atom/AccountTitleText.tsx"
+import PcSaveBar from "@/components/product-create/molecule/PcSaveBar.tsx"
+import { PcInput } from "@/components/product-create/atom/PcInput.tsx"
+import { PcTextarea } from "@/components/product-create/atom/PcTextarea.tsx"
+import PcTitle from "@/components/product-create/atom/PcTitle.tsx"
+import PcDescription from "@/components/product-create/atom/PcDescription.tsx"
+import FieldLengthCounter from "@/components/product-create/atom/FieldLengthCounter.tsx"
+import { PriceStepper } from "@/components/product-create/atom/PcPriceStepper.tsx"
+import PcSelect from "@/components/product-create/atom/PcSelect.tsx"
+import {
+	createProductFirstSchema,
+	createProductFirstSchemaKeys,
+} from "@/schema/product.ts"
+
+const TITLE_MAX_LENGTH = 50
+const TEXTAREA_MAX_LENGTH = 4096
+
+const aiModelOptions = [
+	{ value: "1", label: "ChatGPT" },
+	{ value: "2", label: "DALLE" },
+]
+
+export default function CreateProductFirstPage() {
+	const { control, register, watch } = useForm({
+		resolver: zodResolver(createProductFirstSchema),
+		mode: "onChange",
+		defaultValues: {
+			llmId: "",
+			productName: "",
+			topCategoryUuid: "",
+			subCategoryUuid: "",
+			prompt: "",
+			description: "",
+			price: 1000,
+			discountRate: 0,
+		},
+	})
+
+	// watch values
+	const productNameWatch = watch(
+		createProductFirstSchemaKeys.productName,
+	).length
+	const promptWatch = watch(createProductFirstSchemaKeys.prompt).length
+	const descriptionWatch = watch(
+		createProductFirstSchemaKeys.description,
+	).length
+
+	// todo: API를 통해서 필요한 값들을 가져와야 합니다.
+	const lastSaved = "Jul 3, 2022 at 3:54 PM"
+
+	return (
+		<form className="flex flex-col gap-4">
+			<AccountTitleText className="w-full">Create New Product</AccountTitleText>
+
+			<div className="mt-6 space-y-4">
+				<PcTitle>AI Model</PcTitle>
+				<Controller
+					name={createProductFirstSchemaKeys.llmId}
+					control={control}
+					rules={{ required: "Theme is required" }}
+					render={({ field }) => (
+						<PcSelect
+							className="w-80"
+							options={aiModelOptions}
+							placeholder="Select AI Model"
+							onValueChange={field.onChange}
+							defaultValue={field.value}
+						/>
+					)}
+				/>
+			</div>
+
+			<PcTitle className="">Name</PcTitle>
+			<FieldLengthCounter
+				maxLength={TITLE_MAX_LENGTH}
+				length={productNameWatch}>
+				<PcInput
+					placeholder="Enter Name"
+					{...register(createProductFirstSchemaKeys.productName)}
+				/>
+			</FieldLengthCounter>
+
+			<div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+				<div className="space-y-4">
+					<PcTitle>Category</PcTitle>
+					<Controller
+						name={createProductFirstSchemaKeys.topCategoryUuid}
+						control={control}
+						rules={{ required: "Theme is required" }}
+						render={({ field }) => (
+							<PcSelect
+								options={aiModelOptions}
+								placeholder="Select AI Model"
+								onValueChange={field.onChange}
+								defaultValue={field.value}
+							/>
+						)}
+					/>
+				</div>
+				<div className="space-y-4">
+					<PcTitle>Subcategory</PcTitle>
+					<Controller
+						name={createProductFirstSchemaKeys.subCategoryUuid}
+						control={control}
+						rules={{ required: "Theme is required" }}
+						render={({ field }) => (
+							<PcSelect
+								options={aiModelOptions}
+								placeholder="Select AI Model"
+								onValueChange={field.onChange}
+								defaultValue={field.value}
+							/>
+						)}
+					/>
+				</div>
+			</div>
+
+			<PcTitle className="mt-4">Prompt</PcTitle>
+			<PcDescription>
+				Enter your prompt text here. Include all the settings you used.
+				<br />
+				Put all variables in [square brackets].
+			</PcDescription>
+			<FieldLengthCounter maxLength={TEXTAREA_MAX_LENGTH} length={promptWatch}>
+				<PcTextarea
+					placeholder="Enter your prompt text here. Include all the settings you used. Put all variables in [square brackets]."
+					{...register(createProductFirstSchemaKeys.prompt)}
+				/>
+			</FieldLengthCounter>
+
+			<PcTitle className="mt-4">Description</PcTitle>
+			<FieldLengthCounter
+				maxLength={TEXTAREA_MAX_LENGTH}
+				length={descriptionWatch}>
+				<PcTextarea
+					placeholder="Enter Description"
+					{...register(createProductFirstSchemaKeys.description)}
+				/>
+			</FieldLengthCounter>
+
+			<PcTitle>Price</PcTitle>
+			<Controller
+				name={createProductFirstSchemaKeys.price}
+				control={control}
+				render={({ field }) => (
+					<PriceStepper
+						className="!mb-12 px-8"
+						min={1000}
+						max={5000}
+						step={500}
+						value={field.value}
+						onChange={(value) => {
+							field.onChange(value)
+						}}
+					/>
+				)}
+			/>
+
+			<PcSaveBar lastSaved={lastSaved} />
+		</form>
+	)
+}
