2f94b0f2e35882e661a6689900f85b06593ae19a
yuseok-kim-edushare <yuseok.kim@edushare.kr>
2024-11-29 16:36:48 +0900

    🚧 Type Fix to dedupe

--- apps/client/src/types/modify/commonModifyTypes.ts
+++
@@ -1,9 +0,0 @@
-export interface CommonModifyType {
-	memberUUID?: string | undefined
-	bannerImageUrl?: string | undefined
-	avatarImageUrl?: string | undefined
-	hashTag?: string | undefined
-	bio?: string | undefined
-	email?: string | undefined
-	nickname?: string | undefined
-}
