62179f1ef9cf5f4e0410a5c28a5c50c00d907f50
ParkSinGyu <singyu13@gmail.com>
2024-11-30 14:43:41 +0900

    JYXB304/feat: CreateProductSecondImagePage.tsx 작업중

--- apps/client/src/app/account/[[...slug]]/page.tsx
+++ apps/client/src/app/account/[[...slug]]/page.tsx
@@ -5,6 +5,7 @@ import ContentWrapper from "@/components/account/template/ContentWrapper.tsx"
 import type { SearchParams } from "@/types/account/searchParams.ts"
 import CreateProductFirstPage from "@/components/product-create/page/CreateProductFirstPage.tsx"
 import CreateProductSecondTextPage from "@/components/product-create/page/CreateProductSecondTextPage.tsx"
+import CreateProductSecondImagePage from "@/components/product-create/page/CreateProductSecondImagePage.tsx"
 
 export default function page({ searchParams }: SearchParams) {
 	// note: queryParam이 없는 경우 overview 사이드바 메뉴가 선택되게 하기 -- 필요에 따라 수정 필요
@@ -13,6 +14,7 @@ export default function page({ searchParams }: SearchParams) {
 		<ContentWrapper queryParams={{ view }}>
 			{view === "create-product" && <CreateProductFirstPage />}
 			{view === "product-list" && <CreateProductSecondTextPage />}
+			{view === "product-category" && <CreateProductSecondImagePage />}
 			{view === "favorites" && <Favorite />}
 			{view === "purchase-ongoing" && <PurchaseIng />}
 			{view === "purchase-completed" && <PurchaseEd />}
--- apps/client/src/components/product-create/page/CreateProductSecondImagePage.tsx
+++ apps/client/src/components/product-create/page/CreateProductSecondImagePage.tsx
@@ -1,8 +1,9 @@
 "use client"
 
-import React from "react"
-import { Controller, useForm } from "react-hook-form"
+import React, { useCallback, useEffect, useState } from "react"
+import { Controller, useFieldArray, useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
+import type { z } from "zod"
 import AccountTitleText from "@/components/common/atom/AccountTitleText.tsx"
 import PcTitle from "@/components/product-create/atom/PcTitle.tsx"
 import {
@@ -12,11 +13,43 @@ import {
 import PcSelect from "@/components/product-create/atom/PcSelect.tsx"
 import { PcInput } from "@/components/product-create/atom/PcInput.tsx"
 import PcDropZone from "@/components/product-create/atom/PcDropZone.tsx"
+import { extractPromptVariables, replaceVariables } from "@/lib/productUtils.ts"
+import PcBaseWrapper from "@/components/product-create/atom/PcBaseWrapper.tsx"
+import PcBoundary from "@/components/product-create/atom/PcBoundary.tsx"
+import PcLabel from "@/components/product-create/atom/PcLabel.tsx"
+import PcButton from "@/components/product-create/atom/PcButton.tsx"
+import PcTextPromptSampleList from "@/components/product-create/molecule/PcTextPromptSampleList.tsx"
+import PcPromptSampleSkeleton from "@/components/product-create/atom/PcPromptSampleSkeleton.tsx"
+import PcSaveBar from "@/components/product-create/molecule/PcSaveBar.tsx"
 
 // interface CreateProductSecondTextPageProps {}
 
-// const testTextPrompt =
-// 	"Please provide a prompt for [the AI model] to generate [the text]."
+interface DropResult {
+	draggableId: string
+	type: string
+	source: {
+		droppableId: string
+		index: number
+	}
+	destination: {
+		droppableId: string
+		index: number
+	} | null
+	reason: "DROP" | "CANCEL"
+}
+
+// Mock API call
+const fetchPrompt = () => {
+	return new Promise<string>((resolve) => {
+		setTimeout(() => {
+			resolve(
+				"Please provide a prompt for [the AI model] to generate [the text].",
+			)
+		}, 1000) // Simulate network delay
+	})
+}
+
+type FormData = z.infer<typeof createProductSecondSchema>
 
 const modelVersion = [
 	{ value: "1", label: "Chat GPT 3.5" },
@@ -24,33 +57,102 @@ const modelVersion = [
 	{ value: "3", label: "Chat GPT 4o" },
 ]
 
+/*
+ * todo:
+ *   1) model version API로 가져오기
+ *   2) prompt API로 가져오기
+ *   3) prompt 가져오는 중에 loading spinner 추가하기
+ *   4) prompt 가져오는 중에 error handling 추가하기
+ *   5) 상품 등록 API 호출하기
+ */
 export default function CreateProductSecondImagePage() {
-	const { control, register } = useForm({
+	const [prompt, setPrompt] = useState("")
+
+	const extractPromptVars = useCallback((_prompt: string) => {
+		return extractPromptVariables(_prompt)
+	}, [])
+
+	useEffect(() => {
+		const getPrompt = async () => {
+			try {
+				const fetchedPrompt = await fetchPrompt()
+				setPrompt(fetchedPrompt)
+				const extractedVars = extractPromptVars(fetchedPrompt)
+				replace(extractedVars)
+			} catch (error) {
+				// Handle error
+			}
+		}
+
+		getPrompt().then(() => {
+			// do nothing
+		})
+	}, [])
+
+	const { register, control, handleSubmit, setValue } = useForm<FormData>({
 		resolver: zodResolver(createProductSecondSchema),
-		mode: "onChange",
 		defaultValues: {
+			promptVars: [],
+			promptResult: "",
 			contents: [],
 			seed: "",
 			llmVersionId: "",
 		},
 	})
-	//
-	// const [prompt, setPrompt] = useState(testTextPrompt)
-	// useEffect(() => {
-	// 	console.log(JSON.stringify(extractPromptVariables(prompt)))
-	// }, [prompt])
+
+	const { fields, replace } = useFieldArray({
+		control,
+		name: "promptVars",
+	})
+
+	const {
+		fields: contentFields,
+		append: appendContent,
+		move,
+		remove,
+	} = useFieldArray({
+		control,
+		name: "contents",
+	})
+
+	const onSubmit = (data: FormData) => {
+		const varObj: Record<string, string> = data.promptVars.reduce(
+			(acc, { name, value }) => {
+				return Object.assign(acc, { [name]: value })
+			},
+			{},
+		)
+
+		const newContent = {
+			name: replaceVariables(prompt, varObj),
+			value: varObj,
+			result: data.promptResult || "",
+		}
+		appendContent(newContent)
+		setValue("promptResult", "")
+		// Reset the values of prompt variables
+		data.promptVars.forEach((variable, index) => {
+			setValue(`promptVars.${index}.value`, "")
+		})
+	}
+
+	// Drag and drop
+	const onDragEnd = (result: DropResult) => {
+		if (!result.destination) return
+
+		move(result.source.index, result.destination.index)
+	}
 
 	return (
 		<form className="flex flex-col gap-4">
 			<AccountTitleText className="w-full">Create New Product</AccountTitleText>
-
+			{/* Model version and Seed*/}
 			<div className="mt-6 grid grid-cols-1 gap-4 md:grid-cols-2">
-				<div className="space-y-4">
+				<PcBaseWrapper>
 					<PcTitle>Model Version</PcTitle>
 					<Controller
 						name={createProductSecondSchemaKeys.llmVersionId}
 						control={control}
-						rules={{ required: "Theme is required" }}
 						render={({ field }) => (
 							<PcSelect
 								options={modelVersion}
@@ -60,20 +162,57 @@ export default function CreateProductSecondImagePage() {
 							/>
 						)}
 					/>
-				</div>
-
-				<div className="space-y-4">
-					<PcTitle>Subcategory</PcTitle>
+				</PcBaseWrapper>
+				<PcBaseWrapper>
+					<PcTitle>Seed</PcTitle>
 					<PcInput
 						placeholder="Enter Model Seed"
 						className="h-9"
 						type="number"
 						{...register(createProductSecondSchemaKeys.seed)}
 					/>
-				</div>
+				</PcBaseWrapper>
 			</div>
+			{/* Sample variables and outputs */}
 
-			<PcDropZone />
+			<PcTitle className="mt-6">Sample Prompt</PcTitle>
+			<PcBoundary>
+				<PcLabel>Sample Input</PcLabel>
+				<div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
+					{fields.map((field, index) => (
+						<PcBaseWrapper className="relative space-y-2" key={field.id}>
+							<PcInput
+								{...register(`promptVars.${index}.value`)}
+								className="text-white"
+								id={`promptVar-${index}`}
+								placeholder={field.name}
+							/>
+						</PcBaseWrapper>
+					))}
+				</div>
+				<PcBaseWrapper className="space-y-2">
+					<PcLabel>Sample Output</PcLabel>
+					<PcDropZone />
+				</PcBaseWrapper>
+				<PcButton
+					className="w-full"
+					type="button"
+					onClick={handleSubmit(onSubmit)}>
+					Submit
+				</PcButton>
+			</PcBoundary>
+
+			<PcTitle className="mt-6">Examples</PcTitle>
+			{contentFields.length > 0 ? (
+				<PcTextPromptSampleList
+					contentFields={contentFields}
+					onDragEnd={onDragEnd}
+					onRemove={remove}
+				/>
+			) : (
+				<PcPromptSampleSkeleton />
+			)}
+			<PcSaveBar className="mt-10" />
 		</form>
 	)
 }
