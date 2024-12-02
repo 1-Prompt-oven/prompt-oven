aefd3e1d86251001e30719f0d6f32ae8ecb3d570
ParkSinGyu <singyu13@gmail.com>
2024-12-02 15:08:39 +0900

    JYXB-351/Feat: 프롬프트 상품 등록 관련 타입 정의

---
+++ apps/client/src/types/product/llmType.ts
@@ -0,0 +1,24 @@
+// LLM 관리 API
+// LLM 관리 관련 API endpoints
+export interface GetLlmRequestType {
+	// path
+	llmId: number
+}
+export interface GetLlmResponseType {
+	// query
+	llmName: string
+}
+
+// LLM 버전 관리
+// LLM 버전 관리 API endpoints
+export interface GetLlmVersionListRequestType {
+	// LLM ID 매칭 정보 path
+	// llmId = 1: Dall-E (image)
+	// llmId = 2: GPT (text)
+	llmId: number
+}
+
+export interface GetLlmVersionListResponseType {
+	llmVersionId: number
+	llmVersionName: string
+}
