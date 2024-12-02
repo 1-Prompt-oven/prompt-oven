686cdce8597cf835bcc0296da550fd6c03726005
ParkSinGyu <singyu13@gmail.com>
2024-12-02 17:57:55 +0900

    JYXB-351/Feat: 변경된 사항에 맞춰 타입 수정 (AccountQueryParams)

--- apps/client/src/components/account/template/ContentWrapper.tsx
+++ apps/client/src/components/account/template/ContentWrapper.tsx
@@ -2,12 +2,12 @@
 
 import * as React from "react"
 import SideMenu from "@/components/account/organism/SideMenu.tsx"
-import type { QueryParams } from "@/types/account/searchParams.ts"
+import type { AccountQueryParams } from "@/types/account/searchParams.ts"
 import { sellerNavs } from "@/lib/navigation.ts"
 
 interface AccountContentWrapperProps {
 	children?: React.ReactNode
-	queryParams: QueryParams
+	queryParams: AccountQueryParams
 }
 
 export default function ContentWrapper({
