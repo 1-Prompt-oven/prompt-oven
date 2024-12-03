5c3df5c7de54667e69f6c856db82b9e5fa579a3b
ParkSinGyu <singyu13@gmail.com>
2024-12-04 00:10:29 +0900

    JYXB-351/Feat: dev에 작업 내용 병합

--- apps/client/src/types/next-auth.d.ts
+++ apps/client/src/types/next-auth.d.ts
@@ -1,36 +1,34 @@
 /* eslint-disable -- Disable ESLint for this file to allow flexible type definitions for next-auth */
-import NextAuth from 'next-auth/next';
-import type { DefaultSession, DefaultUser } from 'next-auth/next';
+import type { DefaultSession, DefaultUser } from "next-auth/next"
 
-declare module 'next-auth' {
-  interface Session {
-    user: {
-      id?: number
-      email: string
-      accesstoken: string
-      refreshtoken: string
-      kakaoAccessToken: string
-      kakaoRefreshToken: string
-      NaverAccessToken: string
-      NaverRefreshToken: string
-      GoogleAccessToken: string
-      GooglefreshToken: string
-      nickname: string
-      role: string
-      memberUUID: string
-      failed: boolean
-    } & DefaultSession['user'];
-  }
+declare module "next-auth" {
+	interface Session {
+		user: {
+			id?: number
+			email: string
+			accesstoken: string
+			refreshtoken: string
+			kakaoAccessToken: string
+			kakaoRefreshToken: string
+			NaverAccessToken: string
+			NaverRefreshToken: string
+			GoogleAccessToken: string
+			GooglefreshToken: string
+			nickname: string
+			role: string
+			memberUUID: string
+			failed: boolean
+		} & DefaultSession["user"]
+	}
 
-  interface User extends DefaultUser {
-    id?: numaber
-    email: string
-    accesstoken: string
-    refreshtoken: string
-    nickname: string
-    role: string
-    memberUUID: string
-    failed: boolean
-  }
+	interface User extends DefaultUser {
+		id?: number
+		email: string
+		accesstoken: string
+		refreshtoken: string
+		nickname: string
+		role: string
+		memberUUID: string
+		failed: boolean
+	}
 }
-
