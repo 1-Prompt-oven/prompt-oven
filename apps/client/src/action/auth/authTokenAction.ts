662083981e15158a789f24d22437620899107236
ParkSinGyu <singyu13@gmail.com>
2024-12-10 12:28:16 +0900

    hotfix: 판매자 등록 후에 role이 갱신이 안되던 문제 해결
    -- 판매자 등록 이후에 새로 token을 리프레쉬 로직을 추가하여 해결함.
    -- 해당 부분 문서로 남기기 !!

--- apps/client/src/action/auth/authTokenAction.ts
+++ apps/client/src/action/auth/authTokenAction.ts
@@ -1,17 +1,19 @@
-import type { RefreshResponse } from '@/types/auth/AuthToken';
-import { actionHandler } from '../actionHandler';
+import type { RefreshResponse } from "@/types/auth/AuthToken"
+import { actionHandler } from "../actionHandler"
 
-export const refreshAccessToken = async (refreshToken: string): Promise<RefreshResponse> => {
-  const response = await actionHandler<RefreshResponse>({
-      name: "refreshAccessToken",
-      url: `/v1/auth/refresh`,
-      options: {
-          method: "GET",
-          cache: "no-cache",
-          headers: {
-              Refreshtoken: `Bearer ${refreshToken}`,
-          },
-      },
-  });
-  return response;
-};
+export const refreshAccessToken = async (
+	refreshToken: string,
+): Promise<RefreshResponse> => {
+	const response = await actionHandler<RefreshResponse>({
+		name: "refreshAccessToken",
+		url: `/v1/auth/refresh`,
+		options: {
+			method: "GET",
+			cache: "no-cache",
+			headers: {
+				Refreshtoken: `Bearer ${refreshToken}`,
+			},
+		},
+	})
+	return response
+}
