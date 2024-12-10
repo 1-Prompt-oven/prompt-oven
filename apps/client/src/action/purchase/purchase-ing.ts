654fb6d9357191e579406c3930e9663781817d4e
jangdebug <jangdebug@naver.com>
2024-12-10 11:02:21 +0900

    If Success Payment, Cart Check Data is delete

--- apps/client/src/action/purchase/purchase-ing.ts
+++ apps/client/src/action/purchase/purchase-ing.ts
@@ -1,18 +1,23 @@
+"use server"
+
 import { getMemberUUID } from "@/lib/api/sessionExtractor"
 import type { PaymentItemType } from "@/types/purchase.ts/purchase-ongoing"
 import {
 	getAllCartNumber,
 	getProductDetail,
 } from "../prompt-detail/getProductDetailData"
+import { deleteCartItemList } from "../cart/cartAction"
 
 export async function getPaymentList(): Promise<PaymentItemType[]> {
+	"use server"
 	const memberUUID = await getMemberUUID()
 	if (!memberUUID) return [] // UUID가 없을 경우 빈 배열 반환
 
 	const res = await getAllCartNumber()
+	const selectedIds = res.filter((item) => item.selected)
 
 	const productDetails = await Promise.all(
-		res.map(async (item) => {
+		selectedIds.map(async (item) => {
 			const detail = await getProductDetail(item.productUuid)
 			// 필요한 속성만 반환
 			return {
@@ -24,4 +29,17 @@ export async function getPaymentList(): Promise<PaymentItemType[]> {
 	)
 
 	return productDetails
+}
+
+export async function allDeleteNoCheckCart() {
+	"use server"
+
+	const cartList = await getAllCartNumber()
+
+	const selectedIds = cartList
+		.filter((item) => item.selected) // selected가 true인 항목 필터링
+		.map((item) => item.id) // id만 추출
+
+	const result = deleteCartItemList(selectedIds)
+	return result
 }
