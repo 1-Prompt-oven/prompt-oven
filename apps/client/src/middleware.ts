8efe58f97778b1d87a080a3cf2636c1ec0d9c379
ParkSinGyu <singyu13@gmail.com>
2024-12-11 10:48:52 +0900

    hotfix: 회원, 비회원이 접근할 수 있는 route 경로는 둘 다 접근할 수 있게 수정, Calendar.tsx와 PopOver.tsx 컴포넌트에 lint 에러가 나는거 무시하는 코드 작성 <-- 나중에 수정 필요

--- apps/client/src/middleware.ts
+++ apps/client/src/middleware.ts
@@ -1,13 +1,18 @@
 import type { NextRequest } from "next/server"
 import { NextResponse } from "next/server"
 import { getToken } from "next-auth/jwt"
-import { withAuthRoutes, withOutAuthRoutes } from "@/config/auth/route.ts"
+import {
+	commonAuthRoutes,
+	withAuthRoutes,
+	withOutAuthRoutes,
+} from "@/config/auth/route.ts"
 import { handleWithAuthRequest } from "@/middleware/commonMiddleware.ts"
 
+// note: 비회원과 회원이 공통으로 접근가능한 route에 대해서는 리다이렉트를 거치지 않는 로직이 추가됨.
 const withAuth = async (req: NextRequest, token: boolean) => {
 	const url = req.nextUrl.clone()
 	const { pathname } = req.nextUrl
-	if (!token) {
+	if (!token && !isCommonRoutes(req)) {
 		url.pathname = withOutAuthRoutes.signIn // 로그인 페이지로 경로 설정
 		// url.basePath = routes.signUp
 		url.search = `callbackUrl=${pathname}`
@@ -17,6 +22,7 @@ const withAuth = async (req: NextRequest, token: boolean) => {
 	return handleWithAuthRequest(req)
 }
 
+// note: 비회원과 회원이 공통으로 접근가능한 route에 대해서는 리다이렉트를 거치지 않는 로직이 추가됨.
 const FALLBACK_URL = "/"
 const withOutAuth = async (
 	req: NextRequest,
@@ -24,7 +30,7 @@ const withOutAuth = async (
 	to: string | null,
 ) => {
 	const url = req.nextUrl.clone()
-	if (token) {
+	if (token && !isCommonRoutes(req)) {
 		url.pathname = to ?? FALLBACK_URL
 		url.search = ""
 
@@ -32,8 +38,18 @@ const withOutAuth = async (
 	}
 }
 
+const isCommonRoutes = (req: NextRequest) => {
+	const { pathname } = req.nextUrl
+	// Normalize the pathname
+	const normalizedPathname = pathname.replace(/\/$/, "").toLowerCase()
+	return commonAuthList
+		.map((route) => route.toLowerCase())
+		.includes(normalizedPathname)
+}
+
 const withAuthList: string[] = Object.values(withAuthRoutes)
 const withOutAuthList: string[] = Object.values(withOutAuthRoutes)
+const commonAuthList: string[] = Object.values(commonAuthRoutes)
 
 export default async function middleware(request: NextRequest) {
 	const token = await getToken({
