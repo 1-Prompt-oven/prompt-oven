439a25193d02016ac8f9d9e9be4432b7b8747ce1
jangdebug <jangdebug@naver.com>
2024-12-13 20:07:28 +0900

    Adjustment to 4 or less in line with Toss Payments metadata limit related to product purchase

--- apps/client/src/components/cart/template/CartTemplate.tsx
+++ apps/client/src/components/cart/template/CartTemplate.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { useState } from "react"
 import { useRouter } from "next/navigation"
 import type { CartItemType } from "@/types/cart/cartTypes"
 import {
@@ -7,6 +8,7 @@ import {
 	deleteCartItemList,
 	postCheckoutData,
 } from "@/action/cart/cartAction"
+import FailBuy from "@/components/common/atom/FailBuyModal"
 import CartItemContainer from "../organism/CartItemContainer"
 import CartCheckout from "../organism/CartCheckout"
 import CartTitle from "../atom/CartTitle"
@@ -23,6 +25,10 @@ function CartTemplate({
 	initialTotalPrice,
 }: CartTemplateProps) {
 	const router = useRouter()
+	const [isOpen, setIsOpen] = useState<boolean>(false)
+	const [selectedItemIds, setSelectedItemIds] = useState<number[]>(
+		initialSelectedItems.map((item) => item.id),
+	) // 체크된 상품 ID 상태 추가
 
 	// 전체 체크 및 해제
 	const handleSelectAll = async (checked: boolean) => {
@@ -34,12 +40,26 @@ function CartTemplate({
 		await Promise.all(
 			updatedItems.map((item) => cartCheckUpdate(item, checked)),
 		)
+
+		// 전체 선택 시 체크된 상품 ID 업데이트
+		if (checked) {
+			setSelectedItemIds(updatedItems.map((item) => item.id))
+		} else {
+			setSelectedItemIds([])
+		}
 	}
 
 	// 개별 체크 및 해제
 	const handleSelectItem = async (item: CartItemType) => {
 		const newItemState = { ...item, selected: !item.selected }
 		await cartCheckUpdate(newItemState, newItemState.selected)
+
+		// 체크된 상품 ID 업데이트
+		if (newItemState.selected) {
+			setSelectedItemIds((prev) => [...prev, newItemState.id]) // 체크 시 추가
+		} else {
+			setSelectedItemIds((prev) => prev.filter((id) => id !== newItemState.id)) // 체크 해제 시 제거
+		}
 	}
 
 	const handleDeleteItems = async (itemIds: number[]) => {
@@ -48,20 +68,25 @@ function CartTemplate({
 	}
 
 	const handleDeleteSelectedItems = () => {
-		const selectedItemIds = initialSelectedItems.map((item) => item.id)
-		handleDeleteItems(selectedItemIds)
+		handleDeleteItems(selectedItemIds) // 체크된 상품 ID 사용
+		setSelectedItemIds([]) // 삭제 후 체크된 상품 ID 초기화
 	}
 
 	const handleCheckout = async () => {
-		if (!initialSelectedItems.length) {
+		if (!selectedItemIds.length) {
+			return
+		} else if (selectedItemIds.length > 4) {
+			setIsOpen(!isOpen)
 			return
 		}
 		try {
-			const items = initialSelectedItems.map((item) => ({
-				productUuid: item.productUuid,
-				productName: item.productName,
-				price: item.price,
-			}))
+			const items = initialItems
+				.filter((item) => selectedItemIds.includes(item.id))
+				.map((item) => ({
+					productUuid: item.productUuid,
+					productName: item.productName,
+					price: item.price,
+				}))
 			const success = await postCheckoutData(items)
 			if (success) {
 				router.push("/account?view=purchase-ongoing")
@@ -86,10 +111,13 @@ function CartTemplate({
 					handleDeleteSelectedItems={handleDeleteSelectedItems}
 				/>
 				<CartCheckout
+					count={selectedItemIds.length}
 					totalPrice={initialTotalPrice}
 					onCheckout={handleCheckout}
 				/>
 			</div>
+
+			{isOpen ? <FailBuy isOpen={isOpen} setIsOpen={setIsOpen} /> : null}
 		</div>
 	)
 }
