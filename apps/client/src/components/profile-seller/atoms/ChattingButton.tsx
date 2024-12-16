d92c17a871ec7ed44bab042854e7332ac21a13e1
ParkSinGyu <singyu13@gmail.com>
2024-12-16 13:25:00 +0900

    hotfix: 상대방과 채팅 시작하기 버튼 로딩 추가

--- apps/client/src/components/profile-seller/atoms/ChattingButton.tsx
+++ apps/client/src/components/profile-seller/atoms/ChattingButton.tsx
@@ -2,7 +2,8 @@
 
 import { useRouter } from "next/navigation"
 import { startTalkWith } from "@/action/chat/chatAction.ts"
-import GradientButton from "@/components/common/atom/GradientButton.tsx"
+import LoadingButton from "@/components/common/atom/LoadingButton.tsx"
+import { cn } from "@/lib/utils.ts"
 
 interface ChattingButtonProps {
 	sellerUuid: string
@@ -21,14 +22,25 @@ export default function ChattingButton({
 
 	return (
 		<section>
-			<GradientButton
+			<LoadingButton
 				type="button"
-				className="h-[40px] w-[100px] px-[20px] py-[10px]"
+				className={cn(
+					"h-[40px] w-[100px] px-[20px] py-[10px]",
+					"text-center text-white",
+					"font-sora text-[15px] font-semibold leading-[20px]",
+					"rounded-[10px]",
+					"transition-all duration-300 ease-in-out",
+					"hover:opacity-90",
+					"focus:ring-2 focus:ring-purple-400 focus:ring-opacity-50",
+					"disabled:opacity-50",
+					"bg-gradient-to-r from-[#A913F9] to-[#9D3D81]", // gradient 값
+				)}
+				isAsync
 				onClick={() => {
 					chatWithPeople(sellerUuid).then()
 				}}>
 				대화하기
-			</GradientButton>
+			</LoadingButton>
 		</section>
 	)
 }
