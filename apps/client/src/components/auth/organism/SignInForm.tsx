55a4808d199402ad0ba8aebcbc8fb9fb76aa2c92
ParkSinGyu <singyu13@gmail.com>
2024-12-18 15:17:05 +0900

    hotfix: remove social buttons

--- apps/client/src/components/auth/organism/SignInForm.tsx
+++ apps/client/src/components/auth/organism/SignInForm.tsx
@@ -96,22 +96,22 @@ function SignInForm() {
 		setLoginType(type)
 	}
 	//social
-	const handleSocialLogin = async (provider: "google" | "kakao" | "naver") => {
-		try {
-			const response = await signIn(provider, {
-				redirect: true,
-				callbackUrl: "/",
-			})
-
-			if (!response?.ok) {
-				setErrorMessage("Social login failed. Please try again.")
-			}
-		} catch (error) {
-			setErrorMessage(
-				"An error occurred during social login. Please try again.",
-			)
-		}
-	}
+	// const handleSocialLogin = async (provider: "google" | "kakao" | "naver") => {
+	// 	try {
+	// 		const response = await signIn(provider, {
+	// 			redirect: true,
+	// 			callbackUrl: "/",
+	// 		})
+	//
+	// 		if (!response?.ok) {
+	// 			setErrorMessage("Social login failed. Please try again.")
+	// 		}
+	// 	} catch (error) {
+	// 		setErrorMessage(
+	// 			"An error occurred during social login. Please try again.",
+	// 		)
+	// 	}
+	// }
 	//showmodal
 	const showSuccessModal = (message: string, onConfirm?: () => void) => {
 		setModalMessage(message)
@@ -215,41 +215,41 @@ function SignInForm() {
 						Log In
 					</Button>
 
-					<p className="text-xs text-white/70 sm:text-sm">Or login with</p>
+					{/*<p className="text-xs text-white/70 sm:text-sm">Or login with</p>*/}
 
-					{/* Social Login */}
-					<div className="flex flex-wrap justify-center gap-3 sm:gap-4 md:max-w-[450px] lg:max-w-[450px]">
-						<Button
-							type="button"
-							onClick={() => handleSocialLogin("naver")}
-							variant="outline"
-							className="h-[40px] w-[100px] rounded-full !bg-white hover:!bg-white/90 sm:h-[40px] sm:w-[120px] md:w-[140px]">
-							<svg className="mr-2 h-5 w-5" viewBox="0 0 24 24">
-								<path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-2 16h-2v-6h2v6zm-1-6.891c-.607 0-1.1-.496-1.1-1.109 0-.612.492-1.109 1.1-1.109s1.1.497 1.1 1.109c0 .613-.493 1.109-1.1 1.109zm8 6.891h-1.998v-2.861c0-1.881-2.002-1.722-2.002 0v2.861h-2v-6h2v1.093c.872-1.616 4-1.736 4 1.548v3.359z" />
-							</svg>
-							Naver
-						</Button>
-						<Button
-							type="button"
-							onClick={() => handleSocialLogin("google")}
-							variant="outline"
-							className="h-[40px] w-[100px] rounded-full !bg-white hover:!bg-white/90 sm:h-[40px] sm:w-[120px] md:w-[140px]">
-							<svg className="mr-2 h-5 w-5" viewBox="0 0 24 24">
-								<path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-2.917 16.083c-2.258 0-4.083-1.825-4.083-4.083s1.825-4.083 4.083-4.083c1.103 0 2.024.402 2.735 1.067l-1.107 1.068c-.304-.292-.834-.63-1.628-.63-1.394 0-2.531 1.155-2.531 2.579 0 1.424 1.138 2.579 2.531 2.579 1.616 0 2.224-1.162 2.316-1.762h-2.316v-1.4h3.855c.036.204.064.408.064.677.001 2.332-1.563 3.988-3.919 3.988zm9.917-3.5h-1.75v1.75h-1.167v-1.75h-1.75v-1.166h1.75v-1.75h1.167v1.75h1.75v1.166z" />
-							</svg>
-							Google
-						</Button>
-						<Button
-							type="button"
-							onClick={() => handleSocialLogin("kakao")}
-							variant="outline"
-							className="h-[40px] w-[100px] rounded-full !bg-white hover:!bg-white/90 sm:h-[40px] sm:w-[120px] md:w-[140px]">
-							<svg className="mr-2 h-5 w-5" viewBox="0 0 24 24">
-								<path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm3 8h-1.35c-.538 0-.65.221-.65.778v1.222h2l-.209 2h-1.791v7h-3v-7h-2v-2h2v-2.308c0-1.769.931-2.692 3.029-2.692h1.971v3z" />
-							</svg>
-							KaKao
-						</Button>
-					</div>
+					{/*/!* Social Login *!/*/}
+					{/*<div className="flex flex-wrap justify-center gap-3 sm:gap-4 md:max-w-[450px] lg:max-w-[450px]">*/}
+					{/*	<Button*/}
+					{/*		type="button"*/}
+					{/*		onClick={() => handleSocialLogin("naver")}*/}
+					{/*		variant="outline"*/}
+					{/*		className="h-[40px] w-[100px] rounded-full !bg-white hover:!bg-white/90 sm:h-[40px] sm:w-[120px] md:w-[140px]">*/}
+					{/*		<svg className="mr-2 h-5 w-5" viewBox="0 0 24 24">*/}
+					{/*			<path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-2 16h-2v-6h2v6zm-1-6.891c-.607 0-1.1-.496-1.1-1.109 0-.612.492-1.109 1.1-1.109s1.1.497 1.1 1.109c0 .613-.493 1.109-1.1 1.109zm8 6.891h-1.998v-2.861c0-1.881-2.002-1.722-2.002 0v2.861h-2v-6h2v1.093c.872-1.616 4-1.736 4 1.548v3.359z" />*/}
+					{/*		</svg>*/}
+					{/*		Naver*/}
+					{/*	</Button>*/}
+					{/*	<Button*/}
+					{/*		type="button"*/}
+					{/*		onClick={() => handleSocialLogin("google")}*/}
+					{/*		variant="outline"*/}
+					{/*		className="h-[40px] w-[100px] rounded-full !bg-white hover:!bg-white/90 sm:h-[40px] sm:w-[120px] md:w-[140px]">*/}
+					{/*		<svg className="mr-2 h-5 w-5" viewBox="0 0 24 24">*/}
+					{/*			<path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-2.917 16.083c-2.258 0-4.083-1.825-4.083-4.083s1.825-4.083 4.083-4.083c1.103 0 2.024.402 2.735 1.067l-1.107 1.068c-.304-.292-.834-.63-1.628-.63-1.394 0-2.531 1.155-2.531 2.579 0 1.424 1.138 2.579 2.531 2.579 1.616 0 2.224-1.162 2.316-1.762h-2.316v-1.4h3.855c.036.204.064.408.064.677.001 2.332-1.563 3.988-3.919 3.988zm9.917-3.5h-1.75v1.75h-1.167v-1.75h-1.75v-1.166h1.75v-1.75h1.167v1.75h1.75v1.166z" />*/}
+					{/*		</svg>*/}
+					{/*		Google*/}
+					{/*	</Button>*/}
+					{/*	<Button*/}
+					{/*		type="button"*/}
+					{/*		onClick={() => handleSocialLogin("kakao")}*/}
+					{/*		variant="outline"*/}
+					{/*		className="h-[40px] w-[100px] rounded-full !bg-white hover:!bg-white/90 sm:h-[40px] sm:w-[120px] md:w-[140px]">*/}
+					{/*		<svg className="mr-2 h-5 w-5" viewBox="0 0 24 24">*/}
+					{/*			<path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm3 8h-1.35c-.538 0-.65.221-.65.778v1.222h2l-.209 2h-1.791v7h-3v-7h-2v-2h2v-2.308c0-1.769.931-2.692 3.029-2.692h1.971v3z" />*/}
+					{/*		</svg>*/}
+					{/*		KaKao*/}
+					{/*	</Button>*/}
+					{/*</div>*/}
 				</div>
 
 				{/* Sign up link */}
